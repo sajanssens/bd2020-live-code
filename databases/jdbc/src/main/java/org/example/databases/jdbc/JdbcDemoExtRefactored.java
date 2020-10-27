2eedede3a76053cb3772abc3fdc3295355191321
bramjanssens <bram.janssens@infosupport.com>
2020-10-27 15:36:20 +0100

    Dao example changed a bit.

--- databases/jdbc/src/main/java/org/example/databases/jdbc/JdbcDemoExtRefactored.java
+++ databases/jdbc/src/main/java/org/example/databases/jdbc/JdbcDemoExtRefactored.java
@@ -6,24 +6,14 @@ import org.slf4j.LoggerFactory;
 public class JdbcDemoExtRefactored {
 
     private final Logger log = LoggerFactory.getLogger(JdbcDemoExtRefactored.class);
+    private final PublisherDao dao = new PublisherDao();
 
     public static void main(String[] args) {
         new JdbcDemoExtRefactored().start();
     }
 
     private void start() {
-
-        PublisherDao dao = new PublisherDao();
-
         dao.findAll().forEach(p -> log.debug(p.toString()));
-
-        // jdbcDemo.insertTransactional(connection, statement);
-        // jdbcDemo.delete(connection, "1");
-        //
-        // // sql injection....:
-        // statement.execute("create table test(id int)");
-        // jdbcDemo.delete(statement, "2; drop table test; --");
-
     }
 
 }
