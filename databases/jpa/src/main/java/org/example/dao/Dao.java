f880d566cc6e689160f5b779683ddd8fd5c82995
bramjanssens <bram.janssens@infosupport.com>
2020-11-04 09:46:15 +0100

    Bit of clarification on Dao.

--- databases/jpa/src/main/java/org/example/dao/Dao.java
+++ databases/jpa/src/main/java/org/example/dao/Dao.java
@@ -4,14 +4,14 @@ import javax.persistence.EntityManager;
 import java.lang.reflect.ParameterizedType;
 import java.util.List;
 
-public abstract class Dao<E, I> { // E: type of entity; T: type of id of entity
+public abstract class Dao<E, I> { // E: type of entity; I: type of id of entity
 
     protected final EntityManager em;
 
     public Dao(EntityManager em) { this.em = em; }
 
     public E get(I id) {
-        return em.find(T(), id);
+        return em.find(E(), id);
     }
 
     public E getDetached(I id) {
@@ -52,19 +52,23 @@ public abstract class Dao<E, I> { // E: type of entity; T: type of id of entity
     }
 
     public List<E> findAll() {
-        return em.createQuery("SELECT e FROM " + typeSimple() + " e ", T()).getResultList();
+        return em.createQuery("SELECT e FROM " + typeSimple() + " e ", E()).getResultList();
     }
 
     public List<E> findAllWithNamedQuery() {
-        return em.createNamedQuery(typeSimple() + ".findAll", T()).getResultList();
+        return em.createNamedQuery(typeSimple() + ".findAll", E()).getResultList();
     }
 
-    private String typeSimple() { return T().getSimpleName(); }
+    private String typeSimple() { return E().getSimpleName(); }
 
+    /**
+     * @return a class instance of the first generic type parameter (E) of this Dao,
+     * e.g. for EmployeeDao<Employee, Long>, it returns Employee.class.
+     */
     @SuppressWarnings("unchecked")
-    private Class<E> T() {
-        return (Class<E>) ((ParameterizedType) getClass()
-                .getGenericSuperclass()).getActualTypeArguments()[0];
+    private Class<E> E() {
+        ParameterizedType thisDaoClass = (ParameterizedType) getClass().getGenericSuperclass();
+        return (Class<E>) thisDaoClass.getActualTypeArguments()[0];
     }
 
 }
