7fcb6a59d5c376c3743384fdca30fe33110249b2
bramjanssens <bram.janssens@infosupport.com>
2020-11-24 16:42:52 +0100

    PUT on ContactsResource added.

--- javaee/rest/src/main/java/org/example/domain/ContactDao.java
+++ javaee/rest/src/main/java/org/example/domain/ContactDao.java
@@ -1,23 +1,43 @@
 package org.example.domain;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+
+import static java.util.stream.Collectors.toMap;
 
 public class ContactDao {
 
-    private final List<Contact> contacts = new ArrayList<>(Arrays.asList(
-            Contact.builder().id("1").firstName("Sammie").surname("Smith").email("sam.smith@music.com").build(),
-            Contact.builder().id("2").firstName("Frank").surname("Muscles").email("frank@muscles.com").build(),
-            Contact.builder().id("3").firstName("Eddy").surname("Valentino").email("eddy@valfam.co.uk").build(),
-            Contact.builder().id("4").firstName("Bram").surname("Janssens").email("s.a.janssens@gmail.com").build()
-    ));
+    private int maxId = 0;
 
-    public List<Contact> getAll() { return contacts; }
+    private final Map<String, Contact> contacts;
 
-    public boolean add(Contact c) { return this.contacts.add(c); }
+    public ContactDao() {
+        List<Contact> contacts = Arrays.asList(
+                Contact.builder().id(nextId()).firstName("Sammie").surname("Smith").email("sam.smith@music.com").build(),
+                Contact.builder().id(nextId()).firstName("Frank").surname("Muscles").email("frank@muscles.com").build(),
+                Contact.builder().id(nextId()).firstName("Eddy").surname("Valentino").email("eddy@valfam.co.uk").build(),
+                Contact.builder().id(nextId()).firstName("Bram").surname("Janssens").email("s.a.janssens@gmail.com").build()
+        );
+
+        this.contacts = contacts.stream().collect(toMap(Contact::getId, c -> c));
+    }
+
+    public Collection<Contact> getAll() { return contacts.values(); }
+
+    public boolean add(Contact c) {
+        if (c.getId() == null) c.setId(nextId());
+        return this.contacts.put(c.getId(), c) == null;
+    }
 
     public boolean remove(String id) {
-        return this.contacts.removeIf(c -> c.getId().equals(id));
+        return this.contacts.remove(id) != null;
     }
+
+    public boolean update(String id, Contact c) {
+        return this.contacts.put(id, c) != null;
+    }
+
+    private String nextId() { return ++maxId + ""; }
 }
--- javaee/rest/src/main/java/org/example/resources/ContactsResource.java
+++ javaee/rest/src/main/java/org/example/resources/ContactsResource.java
@@ -4,7 +4,7 @@ import org.example.domain.Contact;
 import org.example.domain.ContactDao;
 
 import javax.ws.rs.*;
-import java.util.List;
+import java.util.Collection;
 
 @Path("/contacts")
 // http://localhost:9080/rest/api/contacts
@@ -13,9 +13,7 @@ public class ContactsResource implements JsonResource {
     private static final ContactDao dao = new ContactDao();
 
     @GET
-    public List<Contact> getAll() {
-        return dao.getAll();
-    }
+    public Collection<Contact> getAll() { return dao.getAll(); }
 
     @POST
     public Contact post(Contact c) {
@@ -30,6 +28,15 @@ public class ContactsResource implements JsonResource {
     public void delete(@PathParam("id") String id) {
         if (!dao.remove(id)) {
             throw new RuntimeException("Delete contact with id " + id + " failed.");
+        }
+    }
+
+    @PUT @Path("{id}")
+    public Contact put(@PathParam("id") String id, Contact c) {
+        if (dao.update(id, c)) {
+            return c;
+        } else {
+            throw new RuntimeException("Update contact " + c + " failed.");
         }
     }
 
