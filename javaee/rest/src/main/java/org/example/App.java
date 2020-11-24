1c56f719958a0fa9ef0fd43dbd608af6487b2ec3
bramjanssens <bram.janssens@infosupport.com>
2020-11-24 12:53:51 +0100

    HelloWorld extended.

--- javaee/rest/src/main/java/org/example/App.java
+++ javaee/rest/src/main/java/org/example/App.java
@@ -2,8 +2,13 @@ package org.example;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.Set;
 
-@ApplicationPath("resources")
+// http(s)://serveraddress:serverport/context-root/...
+// http://localhost:9080/rest/api/
+@ApplicationPath("api")
 public class App extends Application {
+
+    // dit is onze "main"
 
 }
--- javaee/rest/src/main/java/org/example/resources/HelloWorldResource.java
+++ javaee/rest/src/main/java/org/example/resources/HelloWorldResource.java
@@ -1,17 +1,51 @@
 package org.example.resources;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
 
+// http://localhost:9080/rest/api/helloworld
 @Path("/helloworld")
 public class HelloWorldResource {
 
+    // endpoint @ /helloworld?n=...
     @GET
-    public Response helloworld() {
+    public Response getWithOptionalName(@QueryParam("n") String name) {
+        String n = name != null ? name : "";
         return Response
                 .ok() // header: 200 OK
-                .entity("Hello world!") // content
+                .entity("Hello world! " + n) // content
+                .build();
+    }
+
+    // endpoint @ /helloworld
+    @POST
+    public Response post(String requestBody, @Context UriInfo uriInfo) {
+        String body = requestBody != null ? requestBody : "";
+        UriBuilder uriBuilder = uriInfo.getAbsolutePathBuilder().path("a_new_location");
+        return Response
+                .created(uriBuilder.build()) // header: 201 CREATED at path ...
+                .entity("Hello posted world! " + body) // content
+                .build();
+    }
+
+    // endpoint @ /helloworld/q?n=...
+    @GET @Path("/q")
+    public Response helloworldQ(@QueryParam("n") String name) {
+        return Response
+                .ok() // header: 200 OK
+                .entity("Hello " + name + "!") // content
+                .build();
+    }
+
+    // endpoint @ /helloworld/{some_name}.
+    @GET @Path("{name}")
+    public Response helloworldName(@PathParam("name") String name) {
+        return Response
+                .ok() // header: 200 OK
+                .entity("Hello " + name + "!") // content
                 .build();
     }
 
