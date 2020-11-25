05e9202a7435f1e87b88bb080cc6c5fdd7709d89
bramjanssens <bram.janssens@infosupport.com>
2020-11-25 11:45:31 +0100

    Laptop added as JPA entity.

--- javaee/rest/src/main/java/org/example/domain/Contact.java
+++ javaee/rest/src/main/java/org/example/domain/Contact.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.NamedQuery;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -20,12 +21,12 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class Contact {
 
     @Id
-    private String id;
-// UUID Universally unique ID
+    private String id;  // UUID Universally unique ID
     private String firstName;
     private String surname;
     private String email;
 
+    @ManyToOne
     private Laptop laptop;
 
 }
--- javaee/rest/src/main/java/org/example/domain/Laptop.java
+++ javaee/rest/src/main/java/org/example/domain/Laptop.java
@@ -5,6 +5,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @Data
@@ -12,8 +15,11 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement // to be able to send xml response with JAX-B
 @NoArgsConstructor // idem
 @AllArgsConstructor
+@Entity
+@NamedQuery(name = "Laptop.findAll", query = "select c from Laptop c")
 public class Laptop {
 
+    @Id
     private String id; // UUID Universally unique ID
     private String brand;
     private double price;
